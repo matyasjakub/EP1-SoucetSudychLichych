8af267f563c537b5846b4407f80f6129b7a52ca3
Jakub Matyáš <Matyas.Jakub@stud.vassboskovice.cz>
2024-06-12 11:10:22 +0200

    SoucetSudychLichych3

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,30 +1,31 @@
 Module Module1
-
     Sub Main()
-        Console.Write("Zadejte poèet èísel, která chcete zadat: ")
-        Dim pocetCisel As Integer
-        While Not Integer.TryParse(Console.ReadLine(), pocetCisel) OrElse pocetCisel <= 0
-            Console.WriteLine("Prosím, zadejte platné celé èíslo vìtší než nula.")
-            Console.Write("Zadejte poèet èísel, která chcete zadat: ")
-        End While
-
+        ' Zjistíme, kolik èísel chce uživatel zadat
+        Console.WriteLine("Zadejte poèet èísel:")
+        Dim pocetCisel As Integer = Convert.ToInt32(Console.ReadLine())
+        ' Pole pro uložení èísel
         Dim cisla(pocetCisel - 1) As Integer
-
+        ' Cyklické zadávání èísel
         For i As Integer = 0 To pocetCisel - 1
-            Console.Write($"Zadejte èíslo {i + 1}: ")
-            While Not Integer.TryParse(Console.ReadLine(), cisla(i))
-                Console.WriteLine("Prosím, zadejte platné celé èíslo.")
-                Console.Write($"Zadejte èíslo {i + 1}: ")
-            End While
+            Dim cislo As Integer
+            Do
+                Console.WriteLine("Zadejte nezáporné èíslo " & (i + 1) & ":")
+                cislo = Convert.ToInt32(Console.ReadLine())
+                If cislo >= 0 Then
+                    cisla(i) = cislo
+                    Exit Do
+                Else
+                    Console.WriteLine("Zadaná hodnota musí být nezáporná. Zkuste to znovu.")
+                End If
+            Loop
         Next
-
-        Console.WriteLine("Zadaná èísla jsou:")
+        ' Vypsání zadaných èísel
+        Console.WriteLine("Zadali jste následující èísla:")
         For Each cislo As Integer In cisla
             Console.WriteLine(cislo)
         Next
-
-        Console.WriteLine("Stisknìte jakoukoli klávesu pro ukonèení...")
+        ' Ukonèení programu
+        Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení programu.")
         Console.ReadKey()
     End Sub
-
 End Module
