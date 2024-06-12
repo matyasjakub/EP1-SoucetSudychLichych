d1f080e2568e626df7a1aeb8e20e5b7b11f71c41
Jakub Matyáš <Matyas.Jakub@stud.vassboskovice.cz>
2024-06-12 11:09:48 +0200

    soucetsudychlichych2

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,27 +1,30 @@
-Imports System
-
 Module Module1
 
     Sub Main()
+        Console.Write("Zadejte poèet èísel, která chcete zadat: ")
         Dim pocetCisel As Integer
+        While Not Integer.TryParse(Console.ReadLine(), pocetCisel) OrElse pocetCisel <= 0
+            Console.WriteLine("Prosím, zadejte platné celé èíslo vìtší než nula.")
+            Console.Write("Zadejte poèet èísel, která chcete zadat: ")
+        End While
+
         Dim cisla(pocetCisel - 1) As Integer
 
-        Console.WriteLine("Zadejte poèet èísel, které chcete zadat:")
-        pocetCisel = Integer.Parse(Console.ReadLine())
-
         For i As Integer = 0 To pocetCisel - 1
-            Console.WriteLine("Zadejte èíslo {0}:", i + 1)
-            cisla(i) = Integer.Parse(Console.ReadLine())
+            Console.Write($"Zadejte èíslo {i + 1}: ")
+            While Not Integer.TryParse(Console.ReadLine(), cisla(i))
+                Console.WriteLine("Prosím, zadejte platné celé èíslo.")
+                Console.Write($"Zadejte èíslo {i + 1}: ")
+            End While
         Next
 
         Console.WriteLine("Zadaná èísla jsou:")
-
         For Each cislo As Integer In cisla
             Console.WriteLine(cislo)
         Next
-        Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení programu.")
-        Console.ReadKey()
 
+        Console.WriteLine("Stisknìte jakoukoli klávesu pro ukonèení...")
+        Console.ReadKey()
     End Sub
 
 End Module
