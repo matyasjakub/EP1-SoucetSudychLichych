d8ecdfaa73f36a432269a89cf3bd08eea116ffb4
Jakub Matyáš <Matyas.Jakub@stud.vassboskovice.cz>
2024-06-12 11:17:00 +0200

    soucetsudychlichych 6

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,16 +1,12 @@
 Module Module1
     Sub Main()
-        ' Hlavní smyèka programu, která se opakuje, dokud uživatel neukonèí aplikaci zavøením konzolového okna
         While True
-            ' Zjistíme, kolik èísel chce uživatel zadat
             Console.WriteLine("Zadejte poèet èísel:")
             Dim pocetCisel As Integer = Convert.ToInt32(Console.ReadLine())
-            ' Promìnné pro souèty a poèty sudých a lichých èísel
             Dim soucetSudych As Integer = 0
             Dim soucetLichych As Integer = 0
             Dim pocetSudych As Integer = 0
             Dim pocetLichych As Integer = 0
-            ' Cyklické zadávání èísel
             For i As Integer = 0 To pocetCisel - 1
                 Dim cislo As Integer
                 Do
@@ -30,12 +26,10 @@ Module Module1
                     End If
                 Loop
             Next
-            ' Vypsání výsledkù
             Console.WriteLine("Souèet sudých èísel: " & soucetSudych)
             Console.WriteLine("Poèet sudých èísel: " & pocetSudych)
             Console.WriteLine("Souèet lichých èísel: " & soucetLichych)
             Console.WriteLine("Poèet lichých èísel: " & pocetLichych)
-            ' Upozornìní pro uživatele
             Console.WriteLine("Stisknìte Enter pro nový výpoèet nebo zavøete okno pro ukonèení programu.")
             Console.ReadLine()
         End While
